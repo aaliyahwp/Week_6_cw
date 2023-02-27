bb35d02094c33d298e0e38b24bf3a80a7ee46f6b
Salma <salmaibrahim813@gmail.com>
2023-02-27 22:57:07 +0000

    added comments

--- rps.py
+++ rps.py
@@ -1,13 +1,19 @@
 import random
 
-class RockPaperScissors:
-    def __init__(self):
-        self.choices = ["rock", "paper", "scissors"]
+class RockPaperScissors: # created a class called rockpaperscissors for the game
+
+    # Def is a method that acts as a function inside the class.
+
+    def __init__(self): # the __init__ function initialises
+        self.choices = ["rock", "paper", "scissors"] # These are attributes to the class.
         self.computer_choice = None
         self.player_choice = None
 
+    # Function to allow the computer to choose a random choice from the choices list using the random module
     def get_computer_choice(self):
         self.computer_choice = random.choice(self.choices)
+
+ # player choice using input to select from the list. if something not in the list is inputted it will return an error
 
     def get_player_choice(self):
         valid_choice = False
@@ -18,12 +24,13 @@ class RockPaperScissors:
             else:
                 print("Invalid choice. Try again.")
 
+    # This is the rules of the game using conditional statements.
     def determine_winner(self):
 
         if self.player_choice == self.computer_choice:
             print(f'You have both selected {self.player_choice}. You are tied!')
         elif self.computer_choice == 'Rock' and self.player_choice == 'Scissors':
-            print('Rock smashes Scissors, You lose!')
+            print('Rock smashes Scissors, You lose!') # unsure how to print this.
         elif self.computer_choice == 'Scissors' and self.player_choice == 'Paper':
             print('Scissors cut paper! You lose!')
         elif self.computer_choice == 'Paper' and self.player_choice == 'Rock':
@@ -31,6 +38,7 @@ class RockPaperScissors:
         else:
             print('You have won!')
 
+    # This allows us to output the two chosen weapons and uses determine winner to play the game
     def play(self):
         self.get_computer_choice()
         self.get_player_choice()
@@ -38,7 +46,7 @@ class RockPaperScissors:
         print("Computer chose", self.computer_choice)
         self.determine_winner()
 
-if __name__ == "__main__":
+if __name__ == "__main__": # This is the main trick.
 
     game = RockPaperScissors()
     game.play()
